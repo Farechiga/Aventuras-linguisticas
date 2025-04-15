142ed741e5fb0f2e45a520c146f4cd0b7ba266dd
Farechiga <francisco.arechiga@gmail.com>
2025-04-14 20:55:11 -0700

    Update API_CONFIG.js
    Signed-off-by: Farechiga <francisco.arechiga@gmail.com>

--- API_CONFIG.js
+++ API_CONFIG.js
@@ -1,4 +1,7 @@
 // API_CONFIG.js - Safe to commit to GitHub
 const API_CONFIG = {
-  keyVaultUrl: "https://aventuras-vault.vault.azure.net/"
+    keyVaultUrl: "https://aventuras-vault.vault.azure.net/", 
+    keyVaultSecretName: "GamePassword",  // Secret name for the game password
+    functionKeySecretName: "SpeechApiKey", // Secret name for function key
+    speechKeyVaultSecretName: "SpeechApiKey" // Secret name for speech API
 };
