c0ae7977278ba87fc74bcda9f12201ee51088b84
Farechiga <francisco.arechiga@gmail.com>
2025-04-18 12:17:45 -0700

    Delete API_CONFIG.js
    Signed-off-by: Farechiga <francisco.arechiga@gmail.com>

--- API_CONFIG.js
+++
@@ -1,7 +0,0 @@
-// API_CONFIG.js - Safe to commit to GitHub
-const API_CONFIG = {
-    keyVaultUrl: "https://aventuras-vault.vault.azure.net/", 
-    keyVaultSecretName: "GamePassword",  // Secret name for the game password
-    functionKeySecretName: "SpeechApiKey", // Secret name for function key
-    speechKeyVaultSecretName: "SpeechApiKey" // Secret name for speech API
-};
